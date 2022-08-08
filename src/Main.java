123c2c64ba0927dd43641afc7015ac1b3cbfae3b
Pavel Golubev <pavelgolubew@gmail.com>
2022-08-08 22:34:12 +0300

    Main homework done

--- src/Main.java
+++ src/Main.java
@@ -43,5 +43,7 @@ public class Main {
                 System.out.println(pastYear);
             }
         }
+        System.out.println();
+        System.out.println("------------------------------------");
     }
 }
